f8c9d1bbd3fab9d1b27cb212db6c7538192f8abf
datng <datnat264@gmail.com>
2025-06-14 23:59:20 +0700

    init

--- module.py
+++ module.py
@@ -2,11 +2,17 @@ import math
 import re
 import json
 import collections
+import nltk
 from collections import Counter
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
 from nltk.corpus import wordnet
+
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
 
 stop_words = set(stopwords.words('english'))
 
