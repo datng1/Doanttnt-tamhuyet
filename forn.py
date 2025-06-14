d46f52f2fc3e3ed0047950443298df451eb67f3f
datng <datnat264@gmail.com>
2025-06-15 00:02:39 +0700

    init

--- forn.py
+++ forn.py
@@ -77,4 +77,4 @@ def download_result(folder, filename):
     return send_from_directory(os.path.join("data", folder), filename)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
